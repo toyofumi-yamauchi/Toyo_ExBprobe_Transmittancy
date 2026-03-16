099c022a75cef8fa60849e6a0f698698eb8f1bda
Toyo <ty20@illinois.edu>
2026-03-15 21:33:23 -0400

    Implementing non-uniform velocity vector

--- Transmittancy/Analytical_approach.py
+++ Transmittancy/Analytical_approach.py
@@ -20,9 +20,7 @@ print('Chosen ion: {}'.format(ion_name))
 import _ExB_probe_info as _ExB_probe_info
 classes = [name for name, obj in vars(_ExB_probe_info).items()
            if isinstance(obj, type)]
-myExBprobe = _ExB_probe_info.NRL_ExB_probe_Curved
-myExBprobe = _ExB_probe_info.UIUC_ExB_probe
-myExBprobe = _ExB_probe_info.NRL_ExB_probe_6inLong
+myExBprobe = _ExB_probe_info.Sample_ExB_probe
 print('Chosen ExB probe: {}'.format(myExBprobe))
 
 import _transmittancy_calc as _transmittancy_calc
@@ -35,8 +33,13 @@ G     = g._geometric_const_G(myExBprobe) # Geometric constant, 1/m
 
 N           = 60
 M           = 60
-v_ion_array = np.linspace(1e3,60e3,N)
-vwpra_array = np.linspace(1e3,60e3,M)
+v_min       = 1e3
+v_max       = 120e3
+beta        = 2.0
+eta_v_ion   = np.linspace(0, 1, N)
+eta_vwpra   = np.linspace(0, 1, M)
+v_ion_array = np.flip(np.round((v_min - v_max)*(np.tanh(beta*eta_v_ion) / np.tanh(beta)) + v_max,0))
+vwpra_array = np.flip(np.round((v_min - v_max)*(np.tanh(beta*eta_vwpra) / np.tanh(beta)) + v_max,0))
 Δv_w_array  = m_ion/q_ion*G*vwpra_array**2
 α_x         =  0.0 # the ion incident angle, deg
 α_y         =  0.0 # the ion incident angle, deg
@@ -59,20 +62,22 @@ figure_size = (3.37,3.37)
 
 XX, YY = np.meshgrid(vwpra_array/1e3, v_ion_array/1e3)
 fig, axs = plt.subplots(1,1,figsize=(160/25.4,80/25.4),dpi=DPI,facecolor='w',sharex=True,sharey=True)
-CS = axs.contourf(XX,YY, T.transpose(), levels=np.linspace(0,1.0,101),cmap='viridis')
-axs.plot(vwpra_array/1e3, vwpra_array+Δv_w_array/1e3, 'w--', lw=LW/2)
-axs.plot(vwpra_array/1e3, vwpra_array-Δv_w_array/1e3, 'w--', lw=LW/2)
+CS = axs.pcolormesh(XX,YY, T.transpose(), shading='auto',cmap='viridis',norm=mcolors.Normalize(vmin=0,vmax=1))
+for i in range(M-1): axs.plot((XX[0,i]+XX[0,i+1])/2* np.ones(N),YY[:,0],c='k',ls='-',linewidth=0.1)
+for j in range(N-1): axs.plot( XX[0,:]           ,(YY[j,:]+YY[j+1,:])/2,c='k',ls='-',linewidth=0.1)
+axs.plot(vwpra_array/1e3, (vwpra_array+Δv_w_array)/1e3, 'w--', lw=LW/2)
+axs.plot(vwpra_array/1e3, (vwpra_array-Δv_w_array)/1e3, 'w--', lw=LW/2)
 axs.set_title('{}'.format(ion_name) ,fontsize=FS)
 axs.set_ylabel('$v_{ion}$, km/s',fontsize=FS)
 axs.set_xlabel('$v_w$, km/s',fontsize=FS)
-axs.set_xlim((0,120))
+axs.set_xlim((0,v_max/1e3))
 axs.set_ylim(axs.get_xlim())
-axs.set_xticks(np.arange(0,140,20))
+axs.set_xticks(np.arange(0, v_max/1e3+20, 20))
 axs.set_yticks(axs.get_xticks())
 axs.tick_params(axis='both', which='major', labelsize=FS)
 axs.tick_params(axis='both', which='minor', labelsize=FS)
 axs.set_aspect('equal', adjustable='box')
-clb = plt.colorbar(CS, ax=axs, ticks=np.linspace(0, 1.0, 11),shrink=0.8, pad=0.01)
+clb = plt.colorbar(CS, ax=axs, ticks=np.linspace(0, 1.0, 11),shrink=1, pad=0.02)
 clb.ax.tick_params(labelsize=FS)
 clb.set_label(label='Transmittancy level',size=FS)
 
@@ -81,5 +86,5 @@ T_k[0,1:] = v_ion_array
 T_k[1:,0] = vwpra_array
 T_k[1:,1:] = T
 T_k[0,0] = 'v_w[m/s]\\v_ion[m/s];alpha_x={:.1f}[deg];alpha_y={:.1f}[deg];Analytical;{}'.format(α_x,α_y,datetime.now().strftime('%Y-%m-%d %H-%M-%S'))
-np.savetxt( myExBprobe.directory+'T_{}_a (M,N) = ({},{}), v = [{}, {}] kms {}.txt'.format(                              ion_name,M,N,np.min(v_ion_array)/1e3,np.max(v_ion_array)/1e3,myExBprobe.memo),T_k,fmt='%s',delimiter=',')
+np.savetxt( myExBprobe.directory+'T_{}_a (M,N) = ({},{}), v = [{}, {}] kms {} (α_x,α_y) = ({:.2f}°,{:.2f}°).txt'.format(ion_name,M,N,np.min(v_ion_array)/1e3,np.max(v_ion_array)/1e3,myExBprobe.memo,α_x,α_y),T_k,fmt='%s',delimiter=',')
 plt.savefig(myExBprobe.directory+'T_{}_a (M,N) = ({},{}), v = [{}, {}] kms {} (α_x,α_y) = ({:.2f}°,{:.2f}°).png'.format(ion_name,M,N,np.min(v_ion_array)/1e3,np.max(v_ion_array)/1e3,myExBprobe.memo,α_x,α_y),dpi=DPI, bbox_inches='tight')
