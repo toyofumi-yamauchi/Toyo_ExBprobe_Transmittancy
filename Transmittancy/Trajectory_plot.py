eb1c34182ae0a530e7be74b57c7f3f9f18d096d8
Toyo <ty20@illinois.edu>
2026-05-09 12:37:53 -0400

    Update in Trajectory_plot

--- Transmittancy/Trajectory_plot.py
+++ Transmittancy/Trajectory_plot.py
@@ -30,7 +30,7 @@ G     = g._geometric_const_G(myExBprobe) # Geometric constant, 1/m
 α_max = g._max_incident_angle(myExBprobe) # Max incident angle, deg
 Bxpra = f._Bxpra(myExBprobe) # Practical B-field strength, T
 
-#%%
+#%
 v_ion    = 11.5e3 # ion velocity, m/s
 vwpra    = 11.5e3 # Wien velocity, m/s
 α_x      =  0.0 # the ion incident angle, deg
@@ -55,16 +55,9 @@ t_array  = np.arange(0,t_max,dt) # time array, s
 
 #%
 r_n_array       = np.zeros((N_points,len(t_array),6))
-n_last_array    = np.zeros(N_points)
+n_last_array    = np.zeros( N_points                )
+end_TF_array    = np.zeros( N_points                )
 F_n_array       = np.zeros((N_points,len(t_array),7))
-KE_array        = np.zeros((N_points,len(t_array)))
-PE_array        = np.zeros((N_points,len(t_array)))
-TE_array        = np.zeros((N_points,len(t_array)))
-z_array         = np.linspace(-myExBprobe.l_f/2 - myExBprobe.l_c, myExBprobe.l_f/2 + myExBprobe.l_d,1000)
-Ey_plot         = np.zeros((N_points,len(z_array)))
-Bx_plot         = np.zeros((N_points,len(z_array)))
-Fy_plot         = np.zeros((N_points,len(z_array)))
-fy_plot         = np.zeros((N_points,len(t_array)))
 
 count_lost      = 0
 count_collected = 0
@@ -88,19 +81,7 @@ for n in range(0,N_points):
     count_collected += c_collected
     if c_collected == 1:
        n_collected.append(n)
-
-    TE_ini = 0.5*m_ion/q_ion*v_ion**2 + F_n_array[n,0,6] # eV
-    KE_array[n,:int(n_last_array[n])] = 0.5*m_ion/q_ion*(r_n_array[n,:int(n_last_array[n]),3]**2 + r_n_array[n,:int(n_last_array[n]),4]**2 + r_n_array[n,:int(n_last_array[n]),5]**2) # eV
-    PE_array[n,:int(n_last_array[n])] = F_n_array[n,:int(n_last_array[n]),6]
-    TE_array[n,:int(n_last_array[n])] = KE_array[n,:int(n_last_array[n])] + PE_array[n,:int(n_last_array[n])]
-
-    for l in range(0,len(z_array)):
-       Ey_plot[n,l] = f._E(myExBprobe,x_0[n],y_0[n],z_array[l],vwpra)[1]
-       Bx_plot[n,l] = f._B(myExBprobe,x_0[n],y_0[n],z_array[l])[0]
-    Fy_plot[n,:] = q_ion*(v_z_0[n]*Bx_plot[n,:]+Ey_plot[n,:])
-    fy_plot[n,:int(n_last_array[n])] = q_ion*(r_n_array[n,:int(n_last_array[n]),5]*F_n_array[n,:int(n_last_array[n]),0]+F_n_array[n,:int(n_last_array[n]),4])
-
-TE_array[TE_array == 0] = np.nan
+       end_TF_array[n] = 1
 
 end = datetime.now()
 print('Total computation time : {} s'.format((end - start).total_seconds()))
@@ -108,99 +89,120 @@ print('Average computation time: {} s'.format((end - start).total_seconds()/N_po
 print('Lost     : {} / {}'.format(count_lost ,N_points))
 print('Collected: {} / {}'.format(count_collected,N_points))
 
-#%% Plot
-z_check = -myExBprobe.l_f*0.4
-if f._E(myExBprobe,0,0,z_check)[1]/f._E(myExBprobe,0,0,0)[1] < f._B(myExBprobe,0,0,z_check)[0]/f._B(myExBprobe,0,0,0)[0]:
-    print('B > E: ions deflected -y (require lower v_w)')
-else:
-    print('B < E: ions deflected +y (require higher v_w)')
+import pyvista as pv
+pv.close_all()
+pv.set_jupyter_backend("static")
 
-FS = 8 # font size in point
-MS = 1 # marker size
-LW = 1 # line width 
-DPI = 300 # dot per inch
+def make_polylines_subset(trajectory_data,n_last_array,select_mask,last_is_index=True):
+    """
+    Build a PyVista PolyData with only the trajectories selected by select_mask.
 
-figure_size = (3.25,8)
-fig, axs = plt.subplots(5,1,figsize=figure_size,constrained_layout=True,facecolor='w')
-for n in range(0,N_points):
-    lnE = axs[0].plot(z_array*1e3,                              Ey_plot[n,:]                    /np.max(Ey_plot[n,:]),                          ls='-' , lw=LW,c='tab:red',   label='$E_y(z)/E_{y,0}$')
-    lnB = axs[0].plot(z_array*1e3,                              Bx_plot[n,:]                    /np.min(Bx_plot[n,:]),                          ls='--', lw=LW,c='tab:purple',label='$B_x(z)/B_{x,0}$')
-    axs[0].plot(z_check*1e3,f._E(myExBprobe,0,0,z_check)[1]/f._E(myExBprobe,0,0,0)[1],ls='none',lw=LW,c='tab:red'   ,marker='x',ms=8)
-    axs[0].plot(z_check*1e3,f._B(myExBprobe,0,0,z_check)[0]/f._B(myExBprobe,0,0,0)[0],ls='none',lw=LW,c='tab:purple',marker='x',ms=8)
-    axs
-    axs[1].plot(r_n_array[n,                    0,2]*1e3,r_n_array[n,                 0,0]*1e3,ls='none',lw=LW,c='b',marker='.',ms=5)
-    axs[1].plot(r_n_array[n, int(n_last_array[n]),2]*1e3,r_n_array[n, int(n_last_array[n]),0]*1e3,ls='none',lw=LW,c='r',marker='.',ms=5)
-    axs[1].plot(r_n_array[n,:int(n_last_array[n]),2]*1e3,r_n_array[n,:int(n_last_array[n]),0]*1e3,ls='none',lw=LW,c='k',marker='.',ms=MS)
+    Parameters
+    ----------
+    trajectory_data : array, shape (N_traj, N_step, 6)
+        Trajectory data: x, y, z, vx, vy, vz
+    n_last_array : array, shape (N_traj,)
+        Last valid step index (or number of valid steps)
+    select_mask : boolean array, shape (N_traj,)
+        True for trajectories to include
+    last_is_index : bool
+        If True, n_last_array[i] is the last valid index.
+        If False, n_last_array[i] is the number of valid steps.
+    """
 
-    axs[2].plot(r_n_array[n,               0,2]*1e3,r_n_array[n,               0,1]*1e3,ls='none',lw=LW,c='b',marker='.',ms=5)
-    axs[2].plot(r_n_array[n, int(n_last_array[n]),2]*1e3,r_n_array[n, int(n_last_array[n]),1]*1e3,ls='none',lw=LW,c='r',marker='.',ms=5)
-    axs[2].plot(r_n_array[n,:int(n_last_array[n]),2]*1e3,r_n_array[n,:int(n_last_array[n]),1]*1e3,ls='none',lw=LW,c='k',marker='.',ms=MS)
+    trajectory_data = np.asarray(trajectory_data, dtype=float)
+    n_last_array = np.asarray(n_last_array, dtype=int)
+    select_mask = np.asarray(select_mask, dtype=bool)
 
-    axs[3].plot(t_array[:int(n_last_array[n])],(TE_array[n,:int(n_last_array[n])]/TE_array[0,:int(n_last_array[n])]-1)*100,ls='-', lw=LW,c='k',label='Energy change ($E(t=0) = {:.0f} eV$)'.format(TE_ini))
+    n_traj, n_step, n_components = trajectory_data.shape
 
-    axs[4].plot(t_array[:int(n_last_array[n])],(r_n_array[n,:int(n_last_array[n]),5]/v_ion-1)*100 ,ls='-', lw=LW,c='k',label='Z-velocity change ($v_z$(t=0) = {:.1f} km/s'.format(v_ion/1000))
-    
-axs[0].set_title('E- and B-field normalized along z-axis',fontsize=FS)
-axs[1].set_title('Ion Trajectory in the xz plane',fontsize=FS)
-axs[2].set_title('Ion Trajectory in the yz plane',fontsize=FS)
-axs[3].set_title('Energy Conservation: $Total(t) = KE(t) + PE(t)$',fontsize=FS)
-axs[4].set_title('Z-velocity check: $v_z(t)$',fontsize=FS)
+    all_points = []
+    all_lines = []
+    all_traj_id = []
 
-axs[0].set_ylabel('Normalized field, -',fontsize=FS)
-axs[1].set_ylabel('$x$, mm',fontsize=FS)
-axs[2].set_ylabel('$y$, mm',fontsize=FS)
-axs[3].set_ylabel('$TE(t)/TE(t=0)$-1, %',fontsize=FS)
-axs[4].set_ylabel('$v_z(t)/v_z(t=0)$-1, %',fontsize=FS)
+    point_offset = 0
 
-axs[0].set_ylim((-1.1,1.1))
-axs[1].set_ylim((-myExBprobe.d_m*1e3,myExBprobe.d_m*1e3))
-axs[2].set_ylim((-myExBprobe.d_e*1e3,myExBprobe.d_e*1e3)) # mm
-axs[1].set_ylim((-30,30))
-axs[2].set_ylim((-30,30)) # m
+    for i in range(n_traj):
+        if not select_mask[i]:
+            continue
 
-for ax in np.array([axs[0],axs[1],axs[2]]):
-    ax.set_xlabel('$z$, mm',fontsize=FS)
-    ax.set_xlim((-(myExBprobe.l_f/2+myExBprobe.l_c)*1e3,(myExBprobe.l_f/2+myExBprobe.l_d+0.05)*1e3)) # mm
-    ax.fill_between([-(myExBprobe.l_f/2+myExBprobe.l_c)*1e3,ax.get_xlim()[0]]       ,*ax.get_ylim(),alpha=0.2,color='k'         )
-    ax.fill_between([ (myExBprobe.l_f/2+myExBprobe.l_d)*1e3,ax.get_xlim()[1]]       ,*ax.get_ylim(),alpha=0.2,color='k'         )
-    ax.fill_between([-(myExBprobe.l_f/2+myExBprobe.l_c)*1e3,-(myExBprobe.l_f/2)*1e3],*ax.get_ylim(),alpha=0.2,color='tab:blue'  )
-    ax.fill_between([-(myExBprobe.l_f/2               )*1e3, (myExBprobe.l_f/2)*1e3],*ax.get_ylim(),alpha=0.2,color='tab:orange')
-    ax.fill_between([ (myExBprobe.l_f/2+myExBprobe.l_d)*1e3, (myExBprobe.l_f/2)*1e3],*ax.get_ylim(),alpha=0.2,color='tab:green' )
+        if last_is_index:
+            n_valid = int(n_last_array[i]) + 1
+        else:
+            n_valid = int(n_last_array[i])
 
-axs[1].fill_between([-(myExBprobe.l_f/2+myExBprobe.l_c)*1e3,-(myExBprobe.l_f/2)*1e3], myExBprobe.r_t  *1e3, axs[1].get_ylim()[1],alpha=0.8,color='k') # collimator left
-axs[1].fill_between([-(myExBprobe.l_f/2+myExBprobe.l_c)*1e3,-(myExBprobe.l_f/2)*1e3],-myExBprobe.r_t  *1e3,-axs[1].get_ylim()[1],alpha=0.8,color='k') # collimator right
-axs[1].fill_between([-(myExBprobe.l_m/2               )*1e3, (myExBprobe.l_m/2)*1e3], myExBprobe.d_m/2*1e3, axs[1].get_ylim()[1],alpha=0.8,color='tab:purple') # magnet left
-axs[1].fill_between([-(myExBprobe.l_m/2               )*1e3, (myExBprobe.l_m/2)*1e3],-myExBprobe.d_m/2*1e3,-axs[1].get_ylim()[1],alpha=0.8,color='tab:purple') # magnet right
-axs[1].fill_between([ (myExBprobe.l_f/2+myExBprobe.l_d)*1e3, (myExBprobe.l_f/2)*1e3], myExBprobe.r_t  *1e3, axs[1].get_ylim()[1],alpha=0.8,color='k') # drift tube left
-axs[1].fill_between([ (myExBprobe.l_f/2+myExBprobe.l_d)*1e3, (myExBprobe.l_f/2)*1e3],-myExBprobe.r_t  *1e3,-axs[1].get_ylim()[1],alpha=0.8,color='k') # drift tube right
-axs[2].fill_between([-(myExBprobe.l_f/2+myExBprobe.l_c)*1e3,-(myExBprobe.l_f/2)*1e3], myExBprobe.r_t  *1e3, axs[2].get_ylim()[1],alpha=0.8,color='k') # collimator top
-axs[2].fill_between([-(myExBprobe.l_f/2+myExBprobe.l_c)*1e3,-(myExBprobe.l_f/2)*1e3],-myExBprobe.r_t  *1e3,-axs[2].get_ylim()[1],alpha=0.8,color='k') # collimator bottom
+        n_valid = max(0, min(n_valid, n_step))
 
-axs[1].fill_between([ (myExBprobe.l_f/2               )*1e3,axs[1].get_xlim()[1]], myExBprobe.r_t*1e3, axs[1].get_ylim()[1],alpha=0.8,color='k') # drift tube left
-axs[1].fill_between([ (myExBprobe.l_f/2               )*1e3,axs[1].get_xlim()[1]],-myExBprobe.r_t*1e3,-axs[1].get_ylim()[1],alpha=0.8,color='k') # drift tube right
-axs[2].fill_between([ (myExBprobe.l_f/2               )*1e3,axs[2].get_xlim()[1]], myExBprobe.r_t*1e3, axs[2].get_ylim()[1],alpha=0.8,color='k') # drift tube top
-axs[2].fill_between([ (myExBprobe.l_f/2               )*1e3,axs[2].get_xlim()[1]],-myExBprobe.r_t*1e3,-axs[2].get_ylim()[1],alpha=0.8,color='k') # drift tube bottom
-axs[1].fill_between([-(myExBprobe.l_f/2+myExBprobe.l_c)*1e3,axs[1].get_xlim()[0]], myExBprobe.r_1*1e3, axs[1].get_ylim()[1],alpha=0.8,color='k') # collector
-axs[1].fill_between([-(myExBprobe.l_f/2+myExBprobe.l_c)*1e3,axs[1].get_xlim()[0]],-myExBprobe.r_1*1e3,-axs[1].get_ylim()[1],alpha=0.8,color='k') # collector
-axs[2].fill_between([-(myExBprobe.l_f/2+myExBprobe.l_c)*1e3,axs[2].get_xlim()[0]], myExBprobe.r_1*1e3, axs[2].get_ylim()[1],alpha=0.8,color='k') # collector
-axs[2].fill_between([-(myExBprobe.l_f/2+myExBprobe.l_c)*1e3,axs[2].get_xlim()[0]],-myExBprobe.r_1*1e3,-axs[2].get_ylim()[1],alpha=0.8,color='k') # collector
-axs[1].fill_between([ (myExBprobe.l_f/2+myExBprobe.l_d)*1e3,axs[1].get_xlim()[1]],-myExBprobe.r_4*1e3, myExBprobe.r_4*1e3  ,alpha=0.8,color='gold') # collector
+        if n_valid < 2:
+            continue
 
-axs[2].fill_between(myExBprobe.z_electrodes*1e3, myExBprobe.y_electrodes*1e3,alpha=0.8,color='tab:red',lw=0)
-axs[2].fill_between(myExBprobe.z_electrodes*1e3,-myExBprobe.y_electrodes*1e3,alpha=0.8,color='tab:red',lw=0)
+        points = trajectory_data[i, :n_valid, :3]
 
-t_range_min = 0
-t_range_max = t_max/2
-for ax in np.array([axs[3],axs[4]]):
-    ax.set_xlabel('$t$, sec',fontsize=FS)
-    ax.set_xlim((t_range_min,t_range_max))
+        # optional cleanup in case of NaN/inf
+        valid = np.all(np.isfinite(points), axis=1)
+        points = points[valid]
 
-for ax in axs.flat:
-    ax.xaxis.offsetText.set_fontsize(FS)
-    ax.yaxis.offsetText.set_fontsize(FS)
-    ax.tick_params(axis='both',labelsize=FS)
+        if len(points) < 2:
+            continue
 
-# plt.tight_layout()
-plt.savefig(myExBprobe.directory+'Ion Trajectory ({}, v_ion = {:.1f} kms^-1, v_w = {:.1f} kms^-1).png'.format(myExBprobe.memo, v_ion/1e3, vwpra/1e3),dpi=DPI)
-plt.show()
+        n_pts = len(points)
+
+        all_points.append(points)
+
+        ids = np.arange(point_offset, point_offset + n_pts)
+        line = np.hstack(([n_pts], ids))
+        all_lines.append(line)
+
+        all_traj_id.append(np.full(n_pts, i))
+
+        point_offset += n_pts
+
+    if len(all_points) == 0:
+        return None
+
+    poly = pv.PolyData()
+    poly.points = np.vstack(all_points)
+    poly.lines = np.concatenate(all_lines).astype(np.int64)
+    poly.point_data["trajectory_id"] = np.concatenate(all_traj_id)
+
+    return poly
+
+body_mesh    = pv.read(myExBprobe.directory+myExBprobe.filename_CAD)
+coll_mesh    = pv.read(myExBprobe.directory+myExBprobe.filename_CAD_collector)
+body_surface = body_mesh.extract_surface(algorithm='dataset_surface').triangulate().clean()
+coll_surface = coll_mesh.extract_surface(algorithm='dataset_surface').triangulate().clean()
+clip_origin  = body_surface.center
+try:
+    # Best for closed surface geometry, e.g. STL shells.
+    clipped_body_surface = body_surface.clip_closed_surface(normal="-x",origin=clip_origin)
+    clipped_coll_surface = coll_surface.clip_closed_surface(normal="-x",origin=clip_origin)
+except Exception:
+    # Fallback for non-closed or difficult meshes.
+    clipped_body_surface = body_surface.clip(normal="-x",origin=clip_origin,invert=False)
+    clipped_coll_surface = coll_surface.clip(normal="-x",origin=clip_origin,invert=False)
+
+red_mask = (end_TF_array == 1)
+blue_mask = (end_TF_array == 0)
+trajectory_data  = r_n_array/0.0254  # shape: (N_points, N_step, 6)
+red_lines  = make_polylines_subset(trajectory_data,n_last_array,red_mask ,last_is_index=True)
+blue_lines = make_polylines_subset(trajectory_data,n_last_array,blue_mask,last_is_index=True)
+red_tubes  = None if red_lines  is None else red_lines .tube(radius=myExBprobe.r_1*25.4/10, n_sides=12)
+blue_tubes = None if blue_lines is None else blue_lines.tube(radius=myExBprobe.r_1*25.4/10, n_sides=12)
+
+p = pv.Plotter(window_size=(800, 600))
+p.add_mesh(                           clipped_body_surface,color="lightgray",opacity=1.00,smooth_shading=False,label="Body")
+p.add_mesh(                           clipped_coll_surface,color="gold"     ,opacity=1.00,smooth_shading=False,label="Collector")
+if red_tubes is not None:  p.add_mesh(red_tubes           ,color="red"                   ,smooth_shading=True ,label="Collected ions")
+if blue_tubes is not None: p.add_mesh(blue_tubes          ,color="blue"                  ,smooth_shading=True ,label="Lost ions")
+p.camera_position = "yz" # view the XZ plane
+p.camera.roll      =  180 # rotate image in-plane, optional
+p.camera.azimuth   =   0
+p.camera.elevation =   0
+p.enable_parallel_projection()
+p.camera.zoom(1.1)         # zoom in, optional
+p.add_axes()
+p.show_bounds(grid="front", location="outer", xtitle="X [in]", ytitle="Y [in]", ztitle="Z [in]")
+p.add_legend(bcolor='gray',border=True,loc='lower right')
+p.show(screenshot=myExBprobe.directory+"ion_trajectory_geometry.png")
+p.export_html(myExBprobe.directory+"ion_trajectory_geometry.html")
 
